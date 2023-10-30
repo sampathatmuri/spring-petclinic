6ffe857a50b5ad52578fca25034a2e61cb776d75
Sampath <sampathatmuri31@gmail.com>
2023-10-30 14:52:50 -0400

    Update MySqlIntegrationTests.java

--- src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/MySqlIntegrationTests.java
@@ -16,7 +16,7 @@
 
 package org.springframework.samples.petclinic;
 
-import static org.assertj.core.api.Assertions.assertThat;
+// import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledInNativeImage;
